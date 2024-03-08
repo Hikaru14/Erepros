9d51981b03a6fca6d7ea5faea96ee68ab75e30b3
Hikaru14 <hikaruhatsuse14@gmail.com>
2024-03-08 22:25:10 +0700

    dropdown navbar

--- templates/header.php
+++ templates/header.php
@@ -29,4 +29,13 @@
     <link rel="stylesheet" href="../css/style.css">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        @media screen and (min-width:640px) {
+            .mega-menu{
+                width: 640px;
+                transform: translate3d(-120px, 4px, 0) !important;
+            }
+        }
+    </style>
 </head>
