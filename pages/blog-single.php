0cb7227db64bedb95215f6e486e113feea4e3f30
pitoregia <mahesadanuarta@gmail.com>
2024-03-08 22:59:16 +0700

    organizing navbar and add active link

--- pages/blog-single.php
+++ pages/blog-single.php
@@ -22,25 +22,12 @@
       </div>
     </div>
   </div>
-  <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div class="container">
-      <a class="navbar-brand" href="home.php">Feliciano</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="oi oi-menu"></span> Menu
-      </button>
 
-      <div class="collapse navbar-collapse" id="ftco-nav">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item"><a href="home.php" class="nav-link">Home</a></li>
-          <li class="nav-item"><a href="about.php" class="nav-link">About</a></li>
-          <li class="nav-item"><a href="menu.php" class="nav-link">Menu</a></li>
-          <li class="nav-item active"><a href="blog.php" class="nav-link">Stories</a></li>
-          <li class="nav-item"><a href="contact.php" class="nav-link">Contact</a></li>
-          <li class="nav-item cta"><a href="reservation.php" class="nav-link">Book a table</a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+  <?php
+  $currentPage = 'blog-single';
+  include '../templates/navbar.php';
+  ?>
+
   <!-- END nav -->
 
   <section class="hero-wrap hero-wrap-2" style="background-image: url('../images/bg_3.jpg');" data-stellar-background-ratio="0.5">
