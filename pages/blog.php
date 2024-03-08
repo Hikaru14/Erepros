d069e2355c5efdbaeded48c3de1af56bfe8c9cb2
pitoregia <mahesadanuarta@gmail.com>
2024-03-08 21:54:08 +0700

    add simple pagination feature

--- pages/blog.php
+++ pages/blog.php
@@ -81,139 +81,174 @@
 
   <section class="ftco-section bg-light">
     <div class="container">
-      <div class="row">
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_1.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
+      <div class="ftco-search">
+        <div class="row">
+
+          <div class="tab-content" id="v-pills-tabContent">
+            <div class="tab-pane fade show active" id="v-pills-1" role="tabpanel" aria-labelledby="day-1-tab">
+              <div class="row no-gutters d-flex align-items-stretch">
+
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_1.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_2.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_3.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
               </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
             </div>
-          </div>
-        </div>
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_2.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
+
+
+            <div class="tab-pane fade" id="v-pills-2" role="tabpanel" aria-labelledby="v-pills-day-2-tab">
+              <div class="row no-gutters d-flex align-items-stretch">
+
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_4.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_5.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 ftco-animate">
+                  <div class="blog-entry">
+                    <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_6.jpg')">
+                    </a>
+                    <div class="text pt-3 pb-4 px-4">
+                      <div class="meta">
+                        <div><a href="#">Sept. 06, 2019</a></div>
+                        <div><a href="#">Admin</a></div>
+                      </div>
+                      <h3 class="heading">
+                        <a href="#">Taste the delicious foods in Asia</a>
+                      </h3>
+                      <p class="clearfix">
+                        <a href="#" class="float-left read">Read more</a>
+                        <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
+                      </p>
+                    </div>
+                  </div>
+                </div>
               </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
+
             </div>
           </div>
-        </div>
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_3.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
+
+
+          <div class="container mx-auto ">
+            <div class="row no-gutters my-5">
+              <div class="col text-center justify-content-center">
+                <!-- <div class="block-27"> -->
+                <div class="col-md-12 nav-link-wrap">
+                  <div class="nav nav-pills d-flex text-center" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+                    <!-- <div class="nav d-flex text-center" style="  display: -webkit-box;display: -ms-flexbox;display: flex;padding-left: 0;list-style: none;border-radius: 0.25rem;" id="v-pills-tab" role="tablist" aria-orientation="vertical"> -->
+                    <a class="nav-link ftco-animate active" id="v-pills-1-tab " data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">1</a>
+                    <a class="nav-link ftco-animate" id="v-pills-2-tab" data-toggle="pill" href="#v-pills-2" role="tab" aria-controls="v-pills-2" aria-selected="false">2</a>
+                    <a class="nav-link ftco-animate" id="v-pills-3-tab" data-toggle="pill" href="#v-pills-3" role="tab" aria-controls="v-pills-3" aria-selected="false">3</a>
+                    <a class="nav-link ftco-animate" id="v-pills-4-tab" data-toggle="pill" href="#v-pills-4" role="tab" aria-controls="v-pills-4" aria-selected="false">4</a>
+                    <a class="nav-link ftco-animate" id="v-pills-5-tab" data-toggle="pill" href="#v-pills-5" role="tab" aria-controls="v-pills-5" aria-selected="false">5</a>
+                    <a class="nav-link ftco-animate" id="v-pills-6-tab" data-toggle="pill" href="#v-pills-6" role="tab" aria-controls="v-pills-6" aria-selected="false">6</a>
+                    <!-- </div> -->
+
+                  </div>
+                </div>
+                <!-- </div> -->
               </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
             </div>
           </div>
         </div>
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_4.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
-              </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_5.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
-              </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 ftco-animate">
-          <div class="blog-entry">
-            <a href="blog-single.php" class="block-20" style="background-image: url('../images/image_6.jpg')">
-            </a>
-            <div class="text pt-3 pb-4 px-4">
-              <div class="meta">
-                <div><a href="#">Sept. 06, 2019</a></div>
-                <div><a href="#">Admin</a></div>
-              </div>
-              <h3 class="heading">
-                <a href="#">Taste the delicious foods in Asia</a>
-              </h3>
-              <p class="clearfix">
-                <a href="#" class="float-left read">Read more</a>
-                <a href="#" class="float-right meta-chat"><span class="icon-chat"></span> 3</a>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="row no-gutters my-5">
-          <div class="col text-center">
-            <div class="block-27">
-              <ul id="pagination">
-                <li><a href="#" onclick="prevPage()">&lt;</a></li>
-                <li class="active"><a href="#" onclick="goToPage(1)">1</a></li>
-                <li><a href="#" onclick="goToPage(2)">2</a></li>
-                <li><a href="#" onclick="goToPage(3)">3</a></li>
-                <li><a href="#" onclick="goToPage(4)">4</a></li>
-                <li><a href="#" onclick="goToPage(5)">5</a></li>
-                <li><a href="#" onclick="nextPage()">&gt;</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
   </section>
 
   <?php include '../templates/footer.php' ?>
+
+  <!-- <li><a href="#" onclick="prevPage()">&lt;</a></li>
+                  <li class="active"><a href="#" onclick="goToPage(1)"></a></li> -->
+  <!-- <li><a class="#" id="v-pills-1-tab" data-toggle="pill" href="#v-pills-1" role="tab" aria-controls="v-pills-1" aria-selected="true">2</a></li> -->
+  <!-- <li><a href="#" onclick="goToPage(2)">2</a></li>
+                  <li><a href="#" onclick="goToPage(3)">3</a></li>
+                  <li><a href="#" onclick="goToPage(4)">4</a></li>
+                  <li><a href="#" onclick="goToPage(5)">5</a></li>
+                  <li><a href="#" onclick="nextPage()">&gt;</a></li> -->
